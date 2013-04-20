1f98ae1ba165ba19a3693b447bd1159fe4c9e479
Alexis Jacomy <alexis.jacomy@gmail.com>
2013-04-20 18:59:15 +0200

    merge + added range input polyfill + more modules

--- js/app.js
+++ js/app.js
@@ -1,138 +1,177 @@
 (function() {
   'use strict';
   window.lv = window.lv || {};
-  
-  domino.settings({
-    strict: true,
-    verbose: true
-  });
 
-  var dMin = lv.tools.parseDate('1977-09-01'),
-      dMax = new Date();
+  $(document).ready(function() {
+    domino.settings({
+      strict: true,
+      verbose: true
+    });
 
-  lv.control = new domino({
-    name: 'the-lonely-voyage',
-    properties: [
-      {
-        id: 'date',
-        dispatch: 'dateUpdated',
-        triggers: 'updateDate',
-        type: 'date',
-        value: dMin
-      },
-      {
-        id: 'speed',
-        dispatch: 'speedUpdated',
-        triggers: 'updateSpeed',
-        type: 'number',
-        value: 0 // Days per frame
-      },
-      {
-        id: 'historicEvents',
-        dispatch: 'historicEventsUpdated',
-        triggers: 'updateHistoricEvents',
-        type: 'array',
-        value: []
-      },
-      {
-        id: 'contents',
-        dispatch: 'contentsUpdated',
-        triggers: 'updateContents',
-        type: 'object',
-        value: {}
-      },
-      {
-        id: 'path',
-        dispatch: 'pathUpdated',
-        triggers: 'updatePath',
-        type: 'array',
-        value: []
-      },
-      {
-        id: 'pathIndex',
-        dispatch: 'pathIndexUpdated',
-        triggers: 'updatePathIndex',
-        type: 'object',
-        value: {}
-      },
-      {
-        id: 'direction', // angle only
-        dispatch: 'directionUpdated',
-        triggers: 'updateDirection',
-        type: 'number',
-        value: Math.PI
-      },
-      {
-        id: 'curvature', // radius only
-        dispatch: 'curvatureUpdated',
-        triggers: 'updateCurvature',
-        type: 'number',
-        value: 10e6
-      },
-    ],
-    hacks: [
-      {
-        triggers: 'pathUpdated',
-        method: function() {
-          this.log('HACK: Index path');
+    var dMin = lv.tools.parseDate('1977-09-01'),
+        dMax = new Date();
 
-          var i = 0,
-              l,
-              path = this.get('path'),
-              currentMonth = 0,
-              pathIndex = {},
-              point,
-              nextMonth = 0;
+    lv.control = new domino({
+      name: 'the-lonely-voyage',
+      properties: [
+        // Context:
+        {
+          id: 'date',
+          dispatch: 'dateUpdated',
+          triggers: 'updateDate',
+          type: 'date',
+          value: dMin
+        },
+        {
+          id: 'speed',
+          dispatch: 'speedUpdated',
+          triggers: 'updateSpeed',
+          type: 'number',
+          value: 0 // Days per frame
+        },
+        {
+          id: 'isPlaying',
+          dispatch: 'isPlayingUpdated',
+          triggers: 'updateIsPlaying',
+          type: 'boolean',
+          value: false
+        },
 
-          for (l = path.length; i < l; i++) {
-            point = path[i];
-            currentMonth = nextMonth;
-            nextPoint = path[i+1];
-            if (!nextPoint)
-              break;
-            nextPointDate = lv.tools.newDate(nextPoint[0]);
-            nextPointMonth = lv.tools.getMonthDiff(dMin, nextPointDate);
-            nextMonth = nextPointMonth;
-            if (currentMonth === nextPointMonth && !pathIndex[currentMonth])
-              pathIndex[currentMonth] = point;
-            for (j = currentMonth; j < nextPointMonth; j++) {
-              if (!pathIndex[j])
-                pathIndex[j] = point;
+        // Data:
+        {
+          id: 'historicEvents',
+          dispatch: 'historicEventsUpdated',
+          triggers: 'updateHistoricEvents',
+          type: 'array',
+          value: []
+        },
+        {
+          id: 'contents',
+          dispatch: 'contentsUpdated',
+          triggers: 'updateContents',
+          type: 'object',
+          value: {}
+        },
+        {
+          id: 'path',
+          dispatch: 'pathUpdated',
+          triggers: 'updatePath',
+          type: 'array',
+          value: []
+        },
+        {
+          id: 'pathIndex',
+          dispatch: 'pathIndexUpdated',
+          triggers: 'updatePathIndex',
+          type: 'object',
+          value: {}
+        },
+        {
+          id: 'direction', // angle only
+          dispatch: 'directionUpdated',
+          triggers: 'updateDirection',
+          type: 'number',
+          value: Math.PI
+        },
+        {
+          id: 'curvature', // radius only
+          dispatch: 'curvatureUpdated',
+          triggers: 'updateCurvature',
+          type: 'number',
+          value: 10e6
+        },
+      ],
+      hacks: [
+        {
+          triggers: 'pathUpdated',
+          method: function() {
+            this.log('HACK: Index path');
+            
+            var i = 0,
+                l,
+                path = this.get('path'),
+                currentMonth = 0,
+                pathIndex = {},
+                point,
+                nextMonth = 0;
+
+            for (l = path.length; i < l; i++) {
+              point = path[i];
+              currentMonth = nextMonth;
+              nextPoint = path[i+1];
+              if (!nextPoint)
+                break;
+              nextPointDate = lv.tools.newDate(nextPoint[0]);
+              nextPointMonth = lv.tools.getMonthDiff(dMin, nextPointDate);
+              nextMonth = nextPointMonth;
+              if (currentMonth === nextPointMonth && !pathIndex[currentMonth])
+                pathIndex[currentMonth] = point;
+              for (j = currentMonth; j < nextPointMonth; j++) {
+                if (!pathIndex[j])
+                  pathIndex[j] = point;
+              }
             }
+            this.pathIndex = pathIndex;
           }
-          this.pathIndex = pathIndex;
-        }
-      },
-      {
-        triggers: 'dateUpdated',
-        method: function() {
-          this.log('HACK: Compute speed');
+        },
+        {
+          triggers: 'dateUpdated',
+          method: function() {
+            this.log('HACK: Compute speed');
 
-          // First, let's find the nearest points:
-          // TODO
-        }
-      },
-      {
-        triggers: 'dateUpdated',
-        method: function() {
-          this.log('HACK: Compute speed, direction and angle');
+            // First, let's find the nearest points:
+            // TODO
+          }
+        },
+        {
+          triggers: 'dateUpdated',
+          method: function() {
+            this.log('HACK: Compute speed, direction and angle');
 
-          // First, let's find the nearest points:
-          // TODO
+            // First, let's find the nearest points:
+            // TODO
+          }
         }
-      }
-    ]
+      ],
+      services: [
+        {
+          id: 'historicEvents',
+          url: 'samples/events_random.json',
+          setter: 'historicEvents'
+        }
+      ]
+    });
+
+    // Custom bindings:
+    $(window).resize(function() {
+      lv.control.dispatchEvent('resize');
+    });
+
+    // Instanciate modules:
+    lv.control.addModule(
+      lv.modules.playButton,
+      [
+        $('#timeline')
+      ]
+    );
+
+    lv.control.addModule(
+      lv.modules.timeline,
+      [
+        $('#input-container'),
+        dMin,
+        dMax
+      ]
+    );
+
+    lv.control.addModule(
+      lv.modules.renderer,
+      [
+        $('#visualization')
+      ]
+    );
+
+    // Bootstrap:
+    lv.control.dispatchEvent('resize').request('historicEvents');
   });
-
-  // Custom bindings:
-  $(window).resize(function() {
-    lv.control.dispatchEvent('resize');
-  });
-
-  // Instanciate modules:
-  new lv.modules.timeline(
-    $('#input-container'),
-    dMin,
-    dMax
-  );
 })();
--- js/modules.js
+++ js/modules.js
@@ -1,7 +1,35 @@
 (function() {
   'use strict';
+
   window.lv = window.lv || {};
   lv.modules = lv.modules || {};
+
+  lv.modules.playButton = function(html) {
+    domino.module.call(this);
+    var _self = this,
+        _html = html,
+        _play = $('.play', _html),
+        _pause = $('.pause', _html);
+
+    _play.click(function() {
+      _self.dispatchEvent('updateIsPlaying', {
+        isPlaying: true
+      });
+    });
+
+    _pause.click(function() {
+      _self.dispatchEvent('updateIsPlaying', {
+        isPlaying: false
+      });
+    });
+
+    this.triggers.events.isPlayingUpdated = function(control) {
+      if (control.get('isPlaying'))
+        _html.addClass('playing');
+      else
+        _html.removeClass('playing');
+    };
+  };
 
   lv.modules.timeline = function(html, dMin, dMax) {
     domino.module.call(this);
@@ -15,6 +43,10 @@
                           'max="' + _mMax + '" />'),
         _html = html.append(_input);
 
+    // Range input polyfill:
+    _input.rangeinput();
+    _input = $('input', _html);
+
     _input.change(function() {
       _input.attr('title', lv.tools.prettyDate(getDate()));
     });
@@ -23,13 +55,59 @@
       return lv.tools.getNewDate(dMin, { m: _input.val() });
     };
 
-    function drawCaption() {
+    function setDate(d) {
+      _input.val(lv.tools.getMonthsDiff(dMin, d));
+    };
 
+    function drawCaption() {
+      // TODO
     }
 
     this.html = _html;
     this.triggers.events.resize = function() {
       drawCaption();
     };
+    this.triggers.events.dateUpdated = function(control) {
+    console.log(_input);
+      setDate(control.get('date'));
+    };
+  };
+
+  lv.modules.renderer = function(html) {
+    domino.module.call(this);
+
+    var _self = this,
+        _html = html,
+        _canvas = $('canvas', _html)[0],
+        _ctx = _canvas.getContext('2d');
+
+    function draw(control) {
+      // TODO
+    }
+
+    function resize() {
+      _canvas.width = _html.width();
+      _canvas.height = _html.height();
+    }
+
+    this.triggers.events.dateUpdated = draw;
+  };
+
+  lv.modules.contents = function(html) {
+    domino.module.call(this);
+
+    var _self = this,
+        _html = html;
+
+    // TODO
+  };
+
+  lv.modules.rolodex = function(html) {
+    domino.module.call(this);
+
+    var _self = this,
+        _html = html;
+
+    // TODO
   };
 })();
